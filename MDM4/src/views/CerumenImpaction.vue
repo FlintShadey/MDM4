81dab066dcb0412cb6a11ef6a1b9ed504f5206ad
FlintShadey <125614527+FlintShadey@users.noreply.github.com>
2025-08-07 14:24:08 +0000

    Add new diagnosis buttons to CerumenImpaction view
    - Added 'acute ear pain' button
    - Added 'otalgia with possible foreign body' button
    - Added 'rule-out tympanic membrane rupture' button
    - All buttons follow existing pattern and append to secondary text area

--- MDM4/src/views/CerumenImpaction.vue
+++ MDM4/src/views/CerumenImpaction.vue
@@ -8,13 +8,14 @@
     </div>
 
     <div class="text-end mt-3">
-      <v-btn color="#b85fb2" @click="setAlternateValue"> Cerumen removal procedure note </v-btn>
+      <v-btn color="#b85fb2" @click="setAlternateValue">
+        Cerumen removal procedure note
+      </v-btn>
     </div>
 
     <!-- Diagnosis BUTTONS TO APPEND TEXT -->
     <div class="mt-5">
-
-            <v-btn
+      <v-btn
         color="#72728a"
         class="ma-2"
         @click="secondaryValue += 'Pain in the left ear; '"
@@ -28,7 +29,7 @@
       >
         pain right
       </v-btn>
-            <v-btn
+      <v-btn
         color="#72728a"
         class="ma-2"
         @click="secondaryValue += 'Impacted cerumen, left ear; '"
@@ -48,6 +49,27 @@
         @click="secondaryValue += 'Impacted cerumen, bilateral; '"
       >
         cerumen bilateral
+      </v-btn>
+      <v-btn
+        color="#665251"
+        class="ma-2"
+        @click="secondaryValue += 'Acute ear pain; '"
+      >
+        acute ear pain
+      </v-btn>
+      <v-btn
+        color="#72728a"
+        class="ma-2"
+        @click="secondaryValue += 'Otalgia with possible foreign body; '"
+      >
+        otalgia with possible foreign body
+      </v-btn>
+      <v-btn
+        color="#665251"
+        class="ma-2"
+        @click="secondaryValue += 'Rule-out tympanic membrane rupture; '"
+      >
+        rule-out tympanic membrane rupture
       </v-btn>
     </div>
     <!-- SECONDARY TEXTAREA -->
@@ -93,11 +115,13 @@ export default {
   data() {
     return {
       // Main text (primary text area)
-      primaryValue: "The patient presents with hearing loss and ear fullness.\n\nThis patient could have otitis media, which could lead to the spreading of the infection to the mastoid bone, however the eardrum is not visible.\n\nIt is also possible that the patient could have otitis externa, which can lead to inflammation of the cartilage of the outer ear, such as perichondritis, potentially resulting in deformity or damage to the cartilage.\n\nIt is also possible that the patient has Meniere's disease, which could account for the hearing loss and fullness.\n\nBarotrauma can result in ear pain, hearing loss, and a feeling of fullness in the ears; however, the patient does not have a history of this.\n\nCurrently, physical exam suggests that this is cerumen impaction, which could lead to conductive hearing loss and ear drum injury.\n\nThe patient is safe for discharge.\n\nThe patient is educated about ear hygiene and advised to follow up with ENT if the problems persist.",
+      primaryValue:
+        "The patient presents with hearing loss and ear fullness.\n\nThis patient could have otitis media, which could lead to the spreading of the infection to the mastoid bone, however the eardrum is not visible.\n\nIt is also possible that the patient could have otitis externa, which can lead to inflammation of the cartilage of the outer ear, such as perichondritis, potentially resulting in deformity or damage to the cartilage.\n\nIt is also possible that the patient has Meniere's disease, which could account for the hearing loss and fullness.\n\nBarotrauma can result in ear pain, hearing loss, and a feeling of fullness in the ears; however, the patient does not have a history of this.\n\nCurrently, physical exam suggests that this is cerumen impaction, which could lead to conductive hearing loss and ear drum injury.\n\nThe patient is safe for discharge.\n\nThe patient is educated about ear hygiene and advised to follow up with ENT if the problems persist.",
       // Secondary text (for appended Diagnosiss or alternative content)
       secondaryValue: "  ",
       // An alternate text for demonstration
-      alternateValue: "Performed by: J. Flint Smith, MD\n\nProcedure: Removal of Ear Cerumen Impaction\n\n Indications: Patient presents with ear fullness and hearing loss.\nOtoscopic examination reveals complete occlusion of the ear canal with cerumen.\n\nExamination: Thoroughly examined the ear canal with an otoscope.\nIrrigation: Gently irrigated the ear canal with normal saline.\n\nCerumen Removal: Cerumen impaction removed successfully with irrigation and a curette, under direct visualization.\n\nPost-procedure: Ear canal inspected for any residual debris.\n\nFindings: Cerumen impaction successfully removed.\nThere is some mild erythema of the ear canal, with no signs of infection.\n\nComplications: None.",
+      alternateValue:
+        "Performed by: J. Flint Smith, MD\n\nProcedure: Removal of Ear Cerumen Impaction\n\n Indications: Patient presents with ear fullness and hearing loss.\nOtoscopic examination reveals complete occlusion of the ear canal with cerumen.\n\nExamination: Thoroughly examined the ear canal with an otoscope.\nIrrigation: Gently irrigated the ear canal with normal saline.\n\nCerumen Removal: Cerumen impaction removed successfully with irrigation and a curette, under direct visualization.\n\nPost-procedure: Ear canal inspected for any residual debris.\n\nFindings: Cerumen impaction successfully removed.\nThere is some mild erythema of the ear canal, with no signs of infection.\n\nComplications: None.",
       // Snackbar controls
       snackbar: false,
       snackbarText: "",
